9fef24119bd81ecd07bd9f478897b2b773c8fea9
yasum2006 <32138091+yasum2006@users.noreply.github.com>
2022-03-10 23:10:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,9 +11,11 @@
 #
 
 # Uncomment a feed source
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+# sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 # Add a feed source
-echo 'src-git passwall https://github.com/yasum2006/luci-app-passwall.git' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/yasum2006/luci-app-passwall.git' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
