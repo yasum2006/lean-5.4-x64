3a7770dbfd07a8e889a9c3160da42c2356c68dd0
yasum2006 <32138091+yasum2006@users.noreply.github.com>
2023-12-17 19:05:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,8 @@
 #
 
 # Uncomment a feed source
-sed -i '$a src-git helloworld https://github.com/yasum2006/helloworld.git' feeds.conf.default
+# sed -i '$a src-git helloworld https://github.com/yasum2006/helloworld.git' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
 
 # Add a feed source
 # echo 'src-git passwall https://github.com/yasum2006/luci-app-passwall.git' >>feeds.conf.default
